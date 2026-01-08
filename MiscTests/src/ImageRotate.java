8b5f8e22972454f27624e7e03eb41c739187841c
Vikas Gond <vikas.uni@gmail.com>
2026-01-08 17:04:43 +0530

    code modify

--- MiscTests/src/ImageRotate.java
+++ MiscTests/src/ImageRotate.java
@@ -9,18 +9,18 @@ public class ImageRotate {
 		
 		for(int i = 0 ; i < arr1.length ; i++){
 			for(int j = 0 ; j < arr1[i].length ; j++){
-				System.out.print(arr1[i][j]+ "  ");
+				System.out.print(arr1[i][j]+"("+i+","+j+")"+ "  ");
 			}
 			System.out.println();
 		}
 		
 		System.out.println("--------------");
 		
-		for(int i = 0 ; i <= arr1[0].length - 1 ; i++){
+		for(int i = 0 ; i < arr1[0].length  ; i++){
 			
 			for(int j = 0 , k = arr1.length -1 ; j < arr1.length ; j++ , k--){
 				
-				System.out.print(arr1[k][i]+ "  ");
+				System.out.print(arr1[k][i]+"("+k+","+i+")"+ "  ");
 			}
 			
 			System.out.println();
